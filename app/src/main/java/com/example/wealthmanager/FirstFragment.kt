231a0c53bef96033a00e8eae9c23e342a7056957
XinRu <xinrut.n@gmail.com>
2021-07-10 23:15:02 +0800

    completed random number firstapp tutorial

--- app/src/main/java/com/example/wealthmanager/FirstFragment.kt
+++ app/src/main/java/com/example/wealthmanager/FirstFragment.kt
@@ -1,10 +1,13 @@
 package com.example.wealthmanager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.wealthmanager.databinding.FragmentFirstBinding
 
@@ -19,6 +22,14 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count: Int = countString.toInt()
+        count ++ // increase current count by 1
+        showCountTextView.text= count.toString() // display new value in the text view
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -32,8 +43,22 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        binding.randomButton.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount: Int = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+
+            findNavController().navigate(action)
+        }
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            val myToast = Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG) // make the toast
+            myToast.show()
+        }
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
         }
     }
 
--- app/src/main/java/com/example/wealthmanager/SecondFragment.kt
+++ app/src/main/java/com/example/wealthmanager/SecondFragment.kt
@@ -1,11 +1,13 @@
 package com.example.wealthmanager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.wealthmanager.databinding.FragmentSecondBinding
 
 /**
@@ -29,9 +31,19 @@ class SecondFragment : Fragment() {
 
     }
 
+    val args: SecondFragmentArgs by navArgs()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val count = args.myArg
+        val countText = getString(R.string.random_heading, count)
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count > 0) randomNumber = random.nextInt(count + 1)
+
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
